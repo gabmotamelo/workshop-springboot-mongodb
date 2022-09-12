2b592ce5686131c418dba9c45bc1003c45c8c7b8
Gabriel Mota <gabrielmota6@hotmail.com>
2022-09-12 15:09:27 -0300

    Atualização de usuário com PUT

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -38,6 +38,17 @@ public class UserService {
         }
     }
 
+    public User update(User obj){
+        User newObj = repo.findById(obj.getId()).get();
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO objDTO){
         return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
